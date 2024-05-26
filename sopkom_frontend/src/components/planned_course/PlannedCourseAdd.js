76c9dfcea472cc4dad6ed7f9d016f61f65b31d23
Artur Kuśka <91992786+4rturK@users.noreply.github.com>
2024-05-26 12:41:10 +0200

    Szczegółowe informacje i dodawanie planowanych kursów bez API

--- sopkom_frontend/src/components/planned_course/PlannedCourseAdd.js
+++ sopkom_frontend/src/components/planned_course/PlannedCourseAdd.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SERVER_URL } from '../constant';
 import { Link } from "react-router-dom";
 
 const PlannedCourseAdd = () => {
   const [formData, setFormData] = useState({
-    prawoJazdyWazneDo: "",
-    imie: "",
-    nazwisko: "",
-    pesel: "",
+    kurs_id: "",
+    kierowcaid: "",
+    autobusid: "",
+    data_przejazdu: "",
   });
 
+  const [coursesData, setCouresData] = useState([]);
+  const [driverData, setDriverData] = useState([]);
+  const [busData, setBusData] = useState([]);
   const [savedMessage, setSavedMessage] = useState("");
 
-  const handleSubmit = async () => {
+//zmienić API
+  const handleSubmit = async (e) => {
     try {
-      const response = await fetch(SERVER_URL + "/api/kierowca/save", {
+      const response = await fetch(SERVER_URL + "/api/nic/save", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -23,88 +27,151 @@ const PlannedCourseAdd = () => {
       });
 
       if (!response.ok) {
-        throw new Error("Failed to add driver");
+        throw new Error("Failed to add planned course");
       }
 
       setTimeout(() => {
-        window.location.href = '/driver';
+        window.location.href = '/planned_course';
       }, 100);
 
-      setSavedMessage("Dodano kierowcę");
+      setSavedMessage("Dodano kurs");
 
     } catch (error) {
-      setSavedMessage("Nie udało się dodać kierowcy");
+      setSavedMessage("Nie udało się dodać kursu");
     }
   };
 
+  const getCourseData = async () => {
+    try {
+      const response = await fetch(SERVER_URL + "/api/kurs/", { method: "GET", credentials: "include" });
+      if (!response.ok) {
+        throw new Error("error on get 2");
+      }
+      const data = await response.json();
+      setCouresData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getDriverData = async () => {
+    try {
+      const response = await fetch(SERVER_URL + "/api/kierowca/", { method: "GET", credentials: "include" });
+      if (!response.ok) {
+        throw new Error("error on get 2");
+      }
+      const data = await response.json();
+      setDriverData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getBusData = async () => {
+    try {
+      const response = await fetch(SERVER_URL + "/api/autobus/", { method: "GET", credentials: "include" });
+      if (!response.ok) {
+        throw new Error("error on get 2");
+      }
+      const data = await response.json();
+      setBusData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    const modifiedValue = name === 'prawoJazdyWazneDo' ? `${value.split('T')[0]}T00:00` : value;
-    setFormData({ ...formData, [name]: modifiedValue });
+    setFormData({ ...formData, [name]: value });
   };
+
+  useEffect(() => {
+    getCourseData();
+    getDriverData();
+    getBusData();
+  }, []);
 
   return (
     <div className="pt-40">
       <p>{savedMessage}</p>
-      <p>dodawanie kursów</p>
+      <h1>Zaplanuj kurs</h1>
       <div className="addForm">
         <form className="addForm" onSubmit={handleSubmit}>
           <table className="addFormat">
             <tbody>
               <tr>
-                <td className="addWhat">Imię:</td>
+                <td className="addWhat">Kurs: </td>
                 <td>
-                  <input className="addInput"
-                    type="text"
-                    name="imie"
-                    value={formData.imie}
+                  <select
+                    name="kurs_id"
+                    className="dropDownCourse"
+                    value={formData.kurs_id}
+                    onChange={handleChange}
+                  >
+                    <option value="">Wybierz kurs</option>
+                    {Array.isArray(coursesData) && coursesData.map((course) => (
+                      <option key={course.kursId} value={course.kursId}>
+                        {course.linia.numer}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <td className="addWhat">Data przejazdu : </td>
+                <td>
+                  <input
+                    className="addInput"
+                    type="datetime-local"
+                    name="data_przejazdu"
+                    value={formData.data_przejazdu}
                     onChange={handleChange}
                     required
                   />
                 </td>
               </tr>
               <tr>
-                <td className="addWhat">Nazwisko:</td>
+                <td className="addWhat">Kierowca:</td>
                 <td>
-                  <input className="addInput"
-                    type="text"
-                    name="nazwisko"
-                    value={formData.nazwisko}
+                  <select
+                    name="kierowcaid"
+                    className="dropDownCourse"
+                    value={formData.kierowcaid}
                     onChange={handleChange}
-                    required
-                  />
+                  >
+                    <option value="">Wybierz kierowcę</option>
+                    {Array.isArray(driverData) && driverData.map((driver) => (
+                      <option key={driver.kierowcaId} value={driver.kierowcaId}>
+                        {driver.kierowcaId} - {driver.imie} {driver.nazwisko}
+                      </option>
+                    ))}
+                  </select>
                 </td>
               </tr>
               <tr>
-                <td className="addWhat">PESEL:</td>
+                <td className="addWhat">Autobus:</td>
                 <td>
-                  <input className="addInput"
-                    type="text"
-                    name="pesel"
-                    value={formData.pesel}
+                  <select
+                    name="autobusid"
+                    className="dropDownCourse"
+                    value={formData.autobusid}
                     onChange={handleChange}
-                    required
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="addWhat">Prawo jazdy ważne do:</td>
-                <td>
-                    <input className="addInput"
-                        type="datetime-local"
-                        name="prawoJazdyWazneDo"
-                        value={formData.prawoJazdyWazneDo}
-                        onChange={handleChange}
-                        required
-                    />
+                  >
+                    <option value="">Wybierz autobus</option>
+                    {Array.isArray(busData) && busData.map((bus) => (
+                      <option key={bus.autbousId} value={bus.autbousId}>
+                        {bus.autbousId} - {bus.numerRejestracyjny}
+                      </option>
+                    ))}
+                  </select>
                 </td>
               </tr>
             </tbody>
           </table>
-          <button className="infoBtn" type="submit">Dodaj kierowcę</button>
+          <button className="infoBtn" type="submit">Dodaj kurs</button>
         </form>
       </div>
-      <Link className="infoBtn" to={`/driver`}>Powrót</Link>
+      <Link className="infoBtn" to={`/planned_course`}>Powrót</Link>
     </div>
   );
 };
--- sopkom_frontend/src/components/planned_course/PlannedCourseInfo.js
+++ sopkom_frontend/src/components/planned_course/PlannedCourseInfo.js
@@ -20,110 +20,244 @@ function extractLastPathComponent(url) {
     return result;
 }
 
+function formatDateTime(dateTimeStr) {
+
+    const [datePart, timePart] = dateTimeStr.split('T');
+    const [day, month, year] = datePart.split('-');
+    const time = timePart.split(' ')[0];
+
+    const validDateTimeStr = `${year}-${month}-${day}T${time}`;
+
+    const date = new Date(validDateTimeStr);
+
+    if (isNaN(date.getTime())) {
+        return 'Invalid Date';
+    }
+
+    const formattedTime = date.toLocaleTimeString();
+    const formattedDate = date.toLocaleDateString();
+
+    return { hour: formattedTime, date: formattedDate };
+}
+
+function DayTime(dateTimeStr) {
+
+    const [datePart, timePart] = dateTimeStr.split('T');
+    const [day, month, year] = datePart.split('-');
+    const time = timePart.split(' ')[0];
+
+    const validDateTimeStr = `${year}-${month}-${day}T${time}`;
+
+    const date = new Date(validDateTimeStr);
+
+    if (isNaN(date.getTime())) {
+        return 'Invalid Date';
+    }
+
+    const formattedTime = date.toLocaleTimeString();
+    const formattedDate = date.toLocaleDateString();
+
+    const back = formattedTime + " " + formattedDate;
+
+    return back;
+}
+
 const PlannedCourseInfo = () => {
-	const [driverData, setDriverData] = useState([]);
+	const [courseData, setCourseData] = useState(
+      {
+        "przejazd_id": 2,
+        "kursid": 1,
+        "data_startu": "05-07-2022T03:13:05 -02:00",
+        "data_konca": "24-12-2016T07:38:53 -01:00",
+        "kierowca": {
+          "imienazwisko": "Imie1 Kierowca1"
+        },
+        "autobus": {
+          "nazwa": "Autobus0",
+          "numer_rejestracyjny": "SK 11231"
+        },
+        "spalanie": 13.4703,
+        "cena_za_litr": 6.5,
+        "dlugosc_trasy": 25.6875,
+        "liczba_biletow": 76,
+        "status": "zapl",
+        "przystanki": [
+          {
+            "przejazdkursprzystanekid": 4,
+            "kolejnosc": 1,
+            "przewidywana_godzinna": "13-03-2015T03:00:08 -01:00",
+            "realna_godzinna": "17-03-2020T05:55:26 -01:00",
+            "nazwa": "Enlow, Kimball Street",
+            "przystanekid": 9
+          },
+          {
+            "przejazdkursprzystanekid": 6,
+            "kolejnosc": 2,
+            "przewidywana_godzinna": "16-01-2018T05:52:19 -01:00",
+            "realna_godzinna": "23-03-2019T01:20:57 -01:00",
+            "nazwa": "Fingerville, Middagh Street",
+            "przystanekid": 6
+          },
+          {
+            "przejazdkursprzystanekid": 10,
+            "kolejnosc": 3,
+            "przewidywana_godzinna": "11-12-2014T01:37:32 -01:00",
+            "realna_godzinna": "01-07-2016T09:59:42 -02:00",
+            "nazwa": "Bodega, Melba Court",
+            "przystanekid": 5
+          },
+          {
+            "przejazdkursprzystanekid": 7,
+            "kolejnosc": 4,
+            "przewidywana_godzinna": "18-07-2022T08:44:04 -02:00",
+            "realna_godzinna": "27-01-2024T05:39:05 -01:00",
+            "nazwa": "Ona, Irving Avenue",
+            "przystanekid": 5
+          },
+          {
+            "przejazdkursprzystanekid": 9,
+            "kolejnosc": 5,
+            "przewidywana_godzinna": "30-10-2014T12:28:34 -01:00",
+            "realna_godzinna": "01-02-2015T04:52:52 -01:00",
+            "nazwa": "Hoehne, Bridgewater Street",
+            "przystanekid": 7
+          }
+        ]
+      }
+    );
     const [savedMessage, setSavedMessage] = useState("");
 
-    const saveChanges = async () => {
-        try {
-            const response = await fetch(SERVER_URL + "/api/kierowca/" + driverData.kierowcaId, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(driverData)
-            });
-            if (!response.ok) {
-                throw new Error("Failed to save changes");
-            }
-            setSavedMessage("Zmiany zostały zapisane pomyślnie!");
-        } catch (error) {
-            setSavedMessage("Zmiany nie zostały zapisane.");
-
-        }
-    };
-
-
-	const getDriverData = async () => {
+    //Trza zmienić API
+	const getCourseData = async () => {
 
 		try {
-			const response = await fetch(SERVER_URL + "/api/kierowca"+extractLastPathComponent(window.location.href), {method: "GET", credentials: "include"});
+			const response = await fetch(SERVER_URL + "/api/nic"+extractLastPathComponent(window.location.href), {method: "GET", credentials: "include"});
 			if(!response.ok){
 				throw new Error("error on get 2");
 			}
 			const data = await response.json();
-			setDriverData(data);
+			setCourseData(data);
+            console.log(data);
 
 		} catch (error){
-
 
 		}
 	}
 
 	 useEffect(()=>{
-		getDriverData();
+		//getCourseData();
+		console.log(courseData);
 	}, []);
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setDriverData(prevData => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
-
-    const deleteDriver = async () => {
+    //Trza zmienić API
+    const deleteCourse = async () => {
             try {
-                const response = await fetch(SERVER_URL + "/api/kierowca/" + driverData.kierowcaId, {
+                const response = await fetch(SERVER_URL + "/api/nic/" + courseData.przejazd_id, {
                     method: "DELETE",
                     headers: {
                         "Content-Type": "application/json"
                     }
                 });
                 if (!response.ok) {
-                    throw new Error("Failed to delete driver");
+                    throw new Error("Failed to delete planned course");
                 }
                 setTimeout(() => {
-                    window.location.href = '/driver';
+                    window.location.href = '/planned_course';
                 }, 100);
 
             } catch (error) {
-                setSavedMessage("Nie udało się usunąć kierowcy.");
+                setSavedMessage("Nie udało się usunąć kursu.");
             }
         };
 
     return (
         <div className="pt-40">
 
-        <p>Szczegóły kursu</p>
+        <h1>Szczegóły kursu:</h1>
 
            <div className="listDiv">
             {savedMessage && <p>{savedMessage}</p>}
 
+                <table className="underListFormat">
+                    <tbody>
+                        <tr>
+                            <th>Zaplanowany start:</th>
+                            <td>{DayTime(courseData.data_startu)}</td>
+                        </tr>
+                        <tr>
+                            <th>Kierowca:</th>
+                            <td>{courseData.kierowca.imienazwisko}</td>
+                        </tr>
+                        <tr>
+                            <th>Autobus:</th>
+                            <td>{courseData.autobus.nazwa}</td>
+                        </tr>
+                        <tr>
+                            <th>Numer rejestracyjny:</th>
+                            <td>{courseData.autobus.numer_rejestracyjny}</td>
+                        </tr>
+                        <tr>
+                            <th>Spalanie:</th>
+                            <td>{courseData.spalanie} l/km</td>
+                        </tr>
+                        <tr>
+                            <th>Cena za litr:</th>
+                            <td>{courseData.cena_za_litr} zł</td>
+                        </tr>
+                        <tr>
+                            <th>Długość trasy:</th>
+                            <td>{courseData.dlugosc_trasy} km</td>
+                        </tr>
+                        <tr>
+                            <th>Liczba biletów:</th>
+                            <td>{courseData.liczba_biletow}</td>
+                        </tr>
+                        <tr>
+                            <th>Status:</th>
+                            <td>{courseData.status}</td>
+                        </tr>
+                    </tbody>
+                </table>
+
             <table className="tableFormat">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Imię</th>
-                        <th>Nazwisko</th>
-                        <th>Wygaśnięcie prawa jazdy</th>
-                        <th>Pesel</th>
+                        <th rowspan="2">Przystanek</th>
+                        <th colSpan="2">Zaplanowany przyjazd</th>
+                        <th colSpan="2">Rzeczywisty przyjazd</th>
+                    </tr>
+                    <tr>
+                        <th>Godzina</th>
+                        <th>Data</th>
+                        <th>Godzina</th>
+                        <th>Data</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>{driverData.kierowcaId}</td>
-                        <td><input className="infoInput" type="text" name="imie" value={driverData.imie || ''} onChange={handleInputChange} /></td>
-                        <td><input className="infoInput" type="text" name="nazwisko" value={driverData.nazwisko || ''} onChange={handleInputChange} /></td>
-                        <td><input className="infoInput" type="text" name="prawoJazdyWazneDo" value={driverData.prawoJazdyWazneDo || ''} onChange={handleInputChange} /></td>
-                        <td><input className="infoInput" type="text" name="pesel" value={driverData.pesel || ''} onChange={handleInputChange} /></td>
-                        <td><button className="infoBtn" onClick={deleteDriver} >Usuń</button></td>
-                    </tr>
+                    {courseData.przystanki && courseData.przystanki
+                    .sort((a, b) => a.kolejnosc - b.kolejnosc)
+                    .map((stop, stopIndex) => {
+                        const plannedArrival = formatDateTime(stop.przewidywana_godzinna);
+                        const actualArrival = formatDateTime(stop.realna_godzinna);
+                        return (
+                            <tr key={stopIndex}>
+                                <td>{stop.kolejnosc}</td>
+                                <td>{plannedArrival.hour}</td>
+                                <td>{plannedArrival.date}</td>
+                                <td>{actualArrival.hour}</td>
+                                <td>{actualArrival.date}</td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
+
+            {courseData.status === "zapl" && (
+                                <button className="infoBtn" onClick={deleteCourse}>Usuń kurs</button>
+            )}
+
             <div>
-            <Link className="infoBtn" to={`/driver`}>Powrót</Link>
-            <button className="infoBtn" onClick={saveChanges} >Zapisz zmiany</button>
+            <Link className="infoBtn" to={`/planned_course`}>Powrót</Link>
             </div>
         </div>
         </div>
