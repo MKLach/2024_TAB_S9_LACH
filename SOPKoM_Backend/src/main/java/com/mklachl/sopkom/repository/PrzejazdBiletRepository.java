e4baf8b2c7db8d65a3541f1430c569bdb6fc2f2a
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:49:59 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/PrzejazdBiletRepository.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/PrzejazdBiletRepository.java
@@ -7,18 +7,49 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/**
+ * Interfejs repository dla encji PrzejazdBilet
+ */
 public interface PrzejazdBiletRepository extends CrudRepository<PrzejazdBilet, Long> {
 
+    /**
+     * Znajduje wszystkie bilety w przejeździe dla danego przejazdu
+     * @param przejazd przejazd
+     * @return Lista biletów w przejeździe
+     */
     List<PrzejazdBilet> findAllByPrzejazd(Przejazd przejazd);
 
+    /**
+     * Znajduje wszystkie bilety w przejeździe dla danego biletu
+     * @param bilet bilet
+     * @return Lista biletów w przejeździe
+     */
     List<PrzejazdBilet> findAllByBilet(Bilet bilet);
 
+    /**
+     * Znajduje wszystkie bilety w przejeździe o cenie większej niż podana
+     * @param cenaBiletu cena biletu
+     * @return Lista biletów w przejeździe
+     */
     List<PrzejazdBilet> findAllByCenaBiletuGreaterThan(float cenaBiletu);
 
+    /**
+     * Znajduje wszystkie bilety w przejeździe o cenie mniejszej niż podana
+     * @param cenaBiletu cena biletu
+     * @return Lista biletów w przejeździe
+     */
     List<PrzejazdBilet> findAllByCenaBiletuLessThan(float cenaBiletu);
 
+    /**
+     * Znajduje wszystkie bilety w przejeździe o dokładnej cenie
+     * @param cenaBiletu cena biletu
+     * @return Lista biletów w przejeździe
+     */
     List<PrzejazdBilet> findAllByCenaBiletu(float cenaBiletu);
 
+    /**
+     * Znajduje wszystkie bilety w przejeździe
+     * @return Lista biletów w przejeździe
+     */
     List<PrzejazdBilet> findAll();
-
 }
