f64d0eb5921e0499559c105e5f225a7b29177215
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 19:28:31 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/entity/Linia.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/entity/Linia.java
@@ -3,66 +3,93 @@ package com.mklachl.sopkom.model.entity;
 import jakarta.persistence.*;
 import java.util.List;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
+/**
+ * Klasa reprezentująca encję Linia.
+ */
 @Entity
 @Table(name = "linie")
 public class Linia {
+
+    /**
+     * Identyfikator linii.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "linia_id")
     private Long liniaId;
 
+    /**
+     * Numer linii.
+     */
     @Column(name = "numer", nullable = false, length = 50)
     private String numer;
 
+    /**
+     * Lista przystanków w linii.
+     */
     @OneToMany(mappedBy = "linia")
     private List<PrzystanekWlini> przystankiWlini;
 
+    /**
+     * Sprawdza, czy linia jest odwracalna.
+     * @return boolean Wartość true, jeśli linia jest odwracalna, w przeciwnym razie false.
+     */
     @Transient
     public boolean odwracalna() {
-    	
-    	for(int i = 0; i < this.przystankiWlini.size(); i++) {
-    		
-    		Przystanek p = this.przystankiWlini.get(i).getPrzystanek();
-    		
-    		if(p.getPrzystanekOdwrotny() == null) {
-    			return false;
-    		}
-    		
-    		
-    		
-    	}
-    	
-    	return true;
+        for (PrzystanekWlini przystanekWlini : this.przystankiWlini) {
+            Przystanek przystanek = przystanekWlini.getPrzystanek();
+            if (przystanek.getPrzystanekOdwrotny() == null) {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    
-    
-    
-    // Getters
+
+    /**
+     * Zwraca identyfikator linii.
+     * @return liniaId Identyfikator linii.
+     */
     public Long getLiniaId() {
         return liniaId;
     }
 
-    public String getNumer() {
-        return numer;
-    }
-
-    public List<PrzystanekWlini> getPrzystankiWlini() {
-        return przystankiWlini;
-    }
-
-    // Setters
+    /**
+     * Ustawia identyfikator linii.
+     * @param liniaId Identyfikator linii.
+     */
     public void setLiniaId(Long liniaId) {
         this.liniaId = liniaId;
     }
 
+    /**
+     * Zwraca numer linii.
+     * @return numer Numer linii.
+     */
+    public String getNumer() {
+        return numer;
+    }
+
+    /**
+     * Ustawia numer linii.
+     * @param numer Numer linii.
+     */
     public void setNumer(String numer) {
         this.numer = numer;
     }
 
-    public void setPrzystankiWlini(List<PrzystanekWlini> przystankiWlini) { this.przystankiWlini = przystankiWlini; }
+    /**
+     * Zwraca listę przystanków w linii.
+     * @return przystankiWlini Lista przystanków w linii.
+     */
+    public List<PrzystanekWlini> getPrzystankiWlini() {
+        return przystankiWlini;
+    }
 
+    /**
+     * Ustawia listę przystanków w linii.
+     * @param przystankiWlini Lista przystanków w linii.
+     */
+    public void setPrzystankiWlini(List<PrzystanekWlini> przystankiWlini) {
+        this.przystankiWlini = przystankiWlini;
+    }
 }
