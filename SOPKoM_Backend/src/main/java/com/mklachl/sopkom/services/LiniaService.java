149c5c36d20e5a2fe7a22aa4ed8b0e3780a57277
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:57:18 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/LiniaService.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/LiniaService.java
@@ -8,15 +8,45 @@ import com.mklachl.sopkom.exceptions.PrzystanekNotFoundException;
 
 import java.util.Optional;
 
+/**
+ * Interfejs serwisu dla obsługi linii
+ */
 public interface LiniaService {
+
+    /**
+     * Zapisuje nową linię na podstawie DTO
+     * @param liniaDto DTO linii
+     * @return zapisana linia
+     * @throws PrzystanekNotFoundException wyjątek braku przystanku
+     */
     Linia saveLinia(LiniaDtoInput liniaDto) throws PrzystanekNotFoundException;
     
+    /**
+     * Aktualizuje istniejącą linię na podstawie DTO
+     * @param liniaDto DTO linii
+     * @return zaktualizowana linia
+     * @throws PrzystanekNotFoundException wyjątek braku przystanku
+     * @throws LiniaNotFoundException wyjątek braku linii
+     */
     Linia patchLinia(LiniaDtoInput liniaDto) throws PrzystanekNotFoundException, LiniaNotFoundException;
     
-    
+    /**
+     * Usuwa linię na podstawie ID
+     * @param id ID linii
+     */
     void deleteLinia(Long id);
 
+    /**
+     * Znajduje linię po ID
+     * @param id ID linii
+     * @return Optional z linią
+     */
     Optional<Linia> findLiniaById(Long id);
 
+    /**
+     * Znajduje linię po numerze
+     * @param numer numer linii
+     * @return znaleziona linia
+     */
     Linia findLiniaByNumer(String numer);
 }
