8f35018be87cf3105497830b02080cd6a87558ba
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:58:47 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/PrzejazdService.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/PrzejazdService.java
@@ -10,58 +10,189 @@ import com.mklachl.sopkom.model.entity.Przejazd;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-//import com.mklachl.sopkom.model.dto.przejazd.PrzejazdDto;
 
+/**
+ * Interfejs serwisu dla obsługi przejazdów
+ */
 public interface PrzejazdService {
 
+    /**
+     * Zapisuje nowy przejazd
+     * @param przejazd encja przejazdu
+     * @return zapisany przejazd
+     */
     Przejazd savePrzejazd(Przejazd przejazd);
 
+    /**
+     * Zapisuje nowy przejazd na podstawie DTO
+     * @param przejazdDto DTO przejazdu
+     * @return zapisany przejazd
+     */
     Przejazd savePrzejazd(PrzejazdDtoInput przejazdDto);
     
+    /**
+     * Aktualizuje przejazd na podstawie DTO
+     * @param przejazdDto DTO przejazdu
+     * @return zaktualizowany przejazd
+     * @throws Exception wyjątek ogólny
+     */
     Przejazd updatePrzejazd(PrzejazdDtoUpdate przejazdDto) throws Exception;
     
+    /**
+     * Znajduje przejazdy po kierowcy
+     * @param kierowca kierowca
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByKierowca(Kierowca kierowca);
 
+    /**
+     * Znajduje przejazdy po autobusie
+     * @param autobus autobus
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByAutobus(Autobus autobus);
 
+    /**
+     * Znajduje przejazdy po kursie
+     * @param kurs kurs
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByKurs(Kurs kurs);
 
+    /**
+     * Znajduje przejazdy po spalaniu
+     * @param spalanie spalanie
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdBySpalanie(float spalanie);
 
+    /**
+     * Znajduje przejazdy po spalaniu mniejszym niż podana wartość
+     * @param spalanie spalanie
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdBySpalanieLessThan(float spalanie);
 
+    /**
+     * Znajduje przejazdy po spalaniu większym niż podana wartość
+     * @param spalanie spalanie
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdBySpalanieGreaterThan(float spalanie);
 
+    /**
+     * Znajduje przejazdy po cenie za litr paliwa
+     * @param cenaZaLitr cena za litr
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByCenaZaLitr(float cenaZaLitr);
 
+    /**
+     * Znajduje przejazdy po cenie za litr paliwa mniejszej niż podana wartość
+     * @param cenaZaLitr cena za litr
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByCenaZaLitrLessThan(float cenaZaLitr);
 
+    /**
+     * Znajduje przejazdy po cenie za litr paliwa większej niż podana wartość
+     * @param cenaZaLitr cena za litr
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByCenaZaLitrGreaterThan(float cenaZaLitr);
 
+    /**
+     * Znajduje przejazdy po długości trasy
+     * @param dlugoscTrasy długość trasy
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDlugoscTrasy(float dlugoscTrasy);
 
+    /**
+     * Znajduje przejazdy po długości trasy mniejszej niż podana wartość
+     * @param dlugoscTrasy długość trasy
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDlugoscTrasyLessThan(float dlugoscTrasy);
 
+    /**
+     * Znajduje przejazdy po długości trasy większej niż podana wartość
+     * @param dlugoscTrasy długość trasy
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDlugoscTrasyGreaterThan(float dlugoscTrasy);
 
+    /**
+     * Znajduje przejazdy po dacie rozpoczęcia
+     * @param dataStartu data rozpoczęcia
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDataStartu(Date dataStartu);
 
+    /**
+     * Znajduje przejazdy po dacie rozpoczęcia wcześniejszej niż podana wartość
+     * @param dataStartu data rozpoczęcia
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDataStartuBefore(Date dataStartu);
 
+    /**
+     * Znajduje przejazdy po dacie rozpoczęcia późniejszej niż podana wartość
+     * @param dataStartu data rozpoczęcia
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDataStartuAfter(Date dataStartu);
 
+    /**
+     * Znajduje przejazdy po dacie zakończenia
+     * @param dataKonca data zakończenia
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDataKonca(Date dataKonca);
 
+    /**
+     * Znajduje przejazdy po dacie zakończenia wcześniejszej niż podana wartość
+     * @param dataKonca data zakończenia
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDataKoncaBefore(Date dataKonca);
 
+    /**
+     * Znajduje przejazdy po dacie zakończenia późniejszej niż podana wartość
+     * @param dataKonca data zakończenia
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdByDataKoncaAfter(Date dataKonca);
 
+    /**
+     * Znajduje przejazdy po kursie i dacie
+     * @param kurs kurs
+     * @param data data
+     * @return lista przejazdów
+     */
     List<Przejazd> findPrzejazdyByKursAndData(Kurs kurs, Date data);
 
+    /**
+     * Znajduje przejazd po ID
+     * @param id ID przejazdu
+     * @return Optional z przejazdem
+     */
     Optional<Przejazd> findPrzejazdById(long id);
     
+    /**
+     * Znajduje wszystkie daty dla kursu
+     * @param kurs kurs
+     * @param count liczba dat do znalezienia
+     * @return lista dat
+     */
     List<Date> findAllDatesForKurs(Kurs kurs, int count);
 
-	Przejazd createDetachedPrzejazd(Kurs kurs, Date dataPrzejazdu);
+    /**
+     * Tworzy odłączony przejazd na podstawie kursu i daty przejazdu
+     * @param kurs kurs
+     * @param dataPrzejazdu data przejazdu
+     * @return odłączony przejazd
+     */
+    Przejazd createDetachedPrzejazd(Kurs kurs, Date dataPrzejazdu);
 
 }
