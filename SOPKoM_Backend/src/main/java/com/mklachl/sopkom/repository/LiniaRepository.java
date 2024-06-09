4e34d363cc9b6a3594af2bfee7e79521bf3971ab
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:48:11 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/LiniaRepository.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/LiniaRepository.java
@@ -1,16 +1,33 @@
 package com.mklachl.sopkom.repository;
+
 import org.springframework.data.repository.CrudRepository;
 import com.mklachl.sopkom.model.entity.Linia;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface LiniaRepository extends CrudRepository<Linia, Long>{
+/**
+ * Interfejs repository dla encji Linia
+ */
+public interface LiniaRepository extends CrudRepository<Linia, Long> {
 
+    /**
+     * Znajduje linię po identyfikatorze
+     * @param id identyfikator
+     * @return Optional z linią
+     */
     Optional<Linia> findById(Long id);
 
+    /**
+     * Znajduje linię po numerze
+     * @param numer numer
+     * @return Linia
+     */
     Linia findByNumer(String numer);
 
+    /**
+     * Znajduje wszystkie linie
+     * @return Lista linii
+     */
     List<Linia> findAll();
-
 }
