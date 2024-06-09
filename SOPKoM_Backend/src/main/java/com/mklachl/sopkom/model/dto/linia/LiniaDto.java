dd33ce6c241d26be131520782cb951ae22c5dffb
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:05:25 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/linia/LiniaDto.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/linia/LiniaDto.java
@@ -1,38 +1,68 @@
 package com.mklachl.sopkom.model.dto.linia;
 
 import com.mklachl.sopkom.model.entity.Linia;
-import java.util.List;
 
+/**
+ * Data Transfer Object (DTO) dla encji Linia.
+ */
 public class LiniaDto {
+
+    /**
+     * Identyfikator linii.
+     */
     protected Long id;
+
+    /**
+     * Numer linii.
+     */
     protected String numer;
 
+    /**
+     * Konstruktor domyślny.
+     */
     public LiniaDto() {
-
     }
 
-    public LiniaDto(Linia Linia) {
-        this.id = Linia.getLiniaId();
-        this.numer = Linia.getNumer();
+    /**
+     * Konstruktor przyjmujący obiekt encji Linia.
+     * @param linia Obiekt encji Linia.
+     */
+    public LiniaDto(Linia linia) {
+        this.id = linia.getLiniaId();
+        this.numer = linia.getNumer();
     }
 
     // Getters and Setters
+
+    /**
+     * Zwraca identyfikator linii.
+     * @return id Identyfikator linii.
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Ustawia identyfikator linii.
+     * @param id Identyfikator linii.
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Zwraca numer linii.
+     * @return numer Numer linii.
+     */
     public String getNumer() {
         return numer;
     }
 
+    /**
+     * Ustawia numer linii.
+     * @param numer Numer linii.
+     */
     public void setNumer(String numer) {
         this.numer = numer;
     }
-
-    
 }
-
