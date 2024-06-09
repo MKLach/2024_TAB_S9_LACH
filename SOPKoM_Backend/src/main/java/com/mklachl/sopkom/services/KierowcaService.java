74fff5d68468ba5c3402efa21f6a891bce9939b4
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:56:42 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/KierowcaService.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/KierowcaService.java
@@ -9,24 +9,74 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interfejs serwisu dla obsługi kierowców
+ */
 public interface KierowcaService {
 
+    /**
+     * Zapisuje nowego kierowcę
+     * @param kierowca encja kierowcy
+     * @return zapisany kierowca
+     */
     Kierowca saveKierowca(Kierowca kierowca);
 
+    /**
+     * Zapisuje nowego kierowcę na podstawie DTO
+     * @param kierowcaDto DTO kierowcy
+     * @return zapisany kierowca
+     */
     Kierowca saveKierowca(KierowcaDto kierowcaDto);
 
+    /**
+     * Znajduje kierowcę po numerze PESEL
+     * @param pesel numer PESEL
+     * @return znaleziony kierowca
+     */
     Kierowca findKierowcaByPesel(String pesel);
 
+    /**
+     * Znajduje kierowcę powiązanego z użytkownikiem
+     * @param user użytkownik
+     * @return znaleziony kierowca
+     */
     Kierowca findKierowcaByUser(User user);
 
+    /**
+     * Znajduje kierowców po imieniu i nazwisku
+     * @param imie imię
+     * @param nazwisko nazwisko
+     * @return lista kierowców
+     */
     List<Kierowca> findKierowcaByImieAndNazwisko(String imie, String nazwisko);
 
+    /**
+     * Znajduje kierowców, których prawo jazdy jest ważne do podanej daty
+     * @param date data
+     * @return lista kierowców
+     */
     List<Kierowca> findKierowcaByPrawoJazdyWazneDoBefore(Date date);
 
+    /**
+     * Znajduje kierowców, których prawo jazdy jest ważne po podanej dacie
+     * @param date data
+     * @return lista kierowców
+     */
     List<Kierowca> findKierowcaByPrawoJazdyWazneDoAfter(Date date);
 
+    /**
+     * Znajduje kierowcę po ID
+     * @param id ID kierowcy
+     * @return Optional z kierowcą
+     */
     Optional<Kierowca> findKierowcaById(Long id);
 
+    /**
+     * Znajduje dostępnych kierowców w podanym przedziale czasowym
+     * @param startTime czas rozpoczęcia
+     * @param endTime czas zakończenia
+     * @return lista dostępnych kierowców
+     */
     List<Kierowca> findKierowcaByAvailability(Date startTime, Date endTime);
 
 }
