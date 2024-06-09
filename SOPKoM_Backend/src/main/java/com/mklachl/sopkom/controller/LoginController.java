e1618c76338e9a7eeb0ceefc66caa04ba7fda6b0
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:37:14 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/controller/LoginController.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/controller/LoginController.java
@@ -3,16 +3,22 @@ package com.mklachl.sopkom.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class LoginController {
 
-  
+    /**
+     * Endpoint do wyświetlania strony logowania.
+     * @return Nazwa widoku strony logowania
+     */
     @GetMapping("/login")
     public String getLoginPage(){
         return "login";
     }
     
+    /**
+     * Endpoint do wyświetlania strony głównej po wylogowaniu.
+     * @return Nazwa widoku strony głównej
+     */
     @GetMapping("/home")
     public String getLogoutPage(){
         return "home";
