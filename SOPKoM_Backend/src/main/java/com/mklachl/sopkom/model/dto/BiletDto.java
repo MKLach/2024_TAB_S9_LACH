4d4c8b1676563d4f4f00770a3c1fcb62a6631d3c
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 19:41:50 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/BiletDto.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/BiletDto.java
@@ -2,13 +2,36 @@ package com.mklachl.sopkom.model.dto;
 
 import com.mklachl.sopkom.model.entity.Bilet;
 
+/**
+ * Data Transfer Object (DTO) dla encji Bilet.
+ */
 public class BiletDto {
+
+    /**
+     * Identyfikator biletu.
+     */
     private Short biletId;
+
+    /**
+     * Cena biletu.
+     */
     private float cena;
+
+    /**
+     * Nazwa biletu.
+     */
     private String nazwa;
 
-    public BiletDto() {}
+    /**
+     * Konstruktor domyślny.
+     */
+    public BiletDto() {
+    }
 
+    /**
+     * Konstruktor przyjmujący obiekt encji Bilet.
+     * @param bilet Obiekt encji Bilet.
+     */
     public BiletDto(Bilet bilet) {
         this.biletId = bilet.getBiletId();
         this.cena = bilet.getCena();
@@ -16,26 +39,51 @@ public class BiletDto {
     }
 
     // Getters and Setters
+
+    /**
+     * Zwraca identyfikator biletu.
+     * @return biletId Identyfikator biletu.
+     */
     public Short getBiletId() {
         return biletId;
     }
 
+    /**
+     * Ustawia identyfikator biletu.
+     * @param biletId Identyfikator biletu.
+     */
     public void setBiletId(Short biletId) {
         this.biletId = biletId;
     }
 
+    /**
+     * Zwraca cenę biletu.
+     * @return cena Cena biletu.
+     */
     public float getCena() {
         return cena;
     }
 
+    /**
+     * Ustawia cenę biletu.
+     * @param cena Cena biletu.
+     */
     public void setCena(float cena) {
         this.cena = cena;
     }
 
+    /**
+     * Zwraca nazwę biletu.
+     * @return nazwa Nazwa biletu.
+     */
     public String getNazwa() {
         return nazwa;
     }
 
+    /**
+     * Ustawia nazwę biletu.
+     * @param nazwa Nazwa biletu.
+     */
     public void setNazwa(String nazwa) {
         this.nazwa = nazwa;
     }
