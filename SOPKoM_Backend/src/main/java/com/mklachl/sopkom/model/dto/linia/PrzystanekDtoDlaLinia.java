e4dee7fe90e32f3da47deebe1d4b9b1c97dbab83
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:07:10 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/linia/PrzystanekDtoDlaLinia.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/linia/PrzystanekDtoDlaLinia.java
@@ -3,51 +3,90 @@ package com.mklachl.sopkom.model.dto.linia;
 import com.mklachl.sopkom.model.dto.PrzystanekDtoFull;
 import com.mklachl.sopkom.model.entity.PrzystanekWlini;
 
+/**
+ * Data Transfer Object (DTO) dla encji PrzystanekWlini, używany w kontekście linii.
+ */
 public class PrzystanekDtoDlaLinia extends PrzystanekDtoFull {
-  
-	private Short kolejnosc;
 
-	private String adres;
-	
-	private Long przystanekwliniId;
-	
-    public PrzystanekDtoDlaLinia(){
+    /**
+     * Kolejność przystanku w linii.
+     */
+    private Short kolejnosc;
 
+    /**
+     * Adres przystanku (miasto i ulica).
+     */
+    private String adres;
+
+    /**
+     * Identyfikator przystanku w linii.
+     */
+    private Long przystanekwliniId;
+
+    /**
+     * Konstruktor domyślny.
+     */
+    public PrzystanekDtoDlaLinia() {
     }
 
-    public PrzystanekDtoDlaLinia(PrzystanekWlini przystanekWlini){
-    	super(przystanekWlini.getPrzystanek());
-    	this.przystanekwliniId = przystanekWlini.getPrzystanekWliniId();
-    	
+    /**
+     * Konstruktor przyjmujący obiekt encji PrzystanekWlini.
+     * @param przystanekWlini Obiekt encji PrzystanekWlini.
+     */
+    public PrzystanekDtoDlaLinia(PrzystanekWlini przystanekWlini) {
+        super(przystanekWlini.getPrzystanek());
+        this.przystanekwliniId = przystanekWlini.getPrzystanekWliniId();
         this.kolejnosc = przystanekWlini.getKolejnosc();
         this.adres = przystanekWlini.getPrzystanek().getMiasto() + ", " + przystanekWlini.getPrzystanek().getUlica();
     }
 
-   
-	public Short getKolejnosc() {
-		return kolejnosc;
-	}
+    // Getters and Setters
 
-	public void setKolejnosc(Short kolejnosc) {
-		this.kolejnosc = kolejnosc;
-	}
+    /**
+     * Zwraca kolejność przystanku w linii.
+     * @return kolejnosc Kolejność przystanku.
+     */
+    public Short getKolejnosc() {
+        return kolejnosc;
+    }
 
-	public String getAdres() {
-		return adres;
-	}
+    /**
+     * Ustawia kolejność przystanku w linii.
+     * @param kolejnosc Kolejność przystanku.
+     */
+    public void setKolejnosc(Short kolejnosc) {
+        this.kolejnosc = kolejnosc;
+    }
 
-	public void setAdres(String adres) {
-		this.adres = adres;
-	}
+    /**
+     * Zwraca adres przystanku.
+     * @return adres Adres przystanku.
+     */
+    public String getAdres() {
+        return adres;
+    }
 
-	public Long getPrzystanekwliniId() {
-		return przystanekwliniId;
-	}
+    /**
+     * Ustawia adres przystanku.
+     * @param adres Adres przystanku.
+     */
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
 
-	public void setPrzystanekwliniId(Long przystanekwliniId) {
-		this.przystanekwliniId = przystanekwliniId;
-	}
-    
-	
-	
+    /**
+     * Zwraca identyfikator przystanku w linii.
+     * @return przystanekwliniId Identyfikator przystanku w linii.
+     */
+    public Long getPrzystanekwliniId() {
+        return przystanekwliniId;
+    }
+
+    /**
+     * Ustawia identyfikator przystanku w linii.
+     * @param przystanekwliniId Identyfikator przystanku w linii.
+     */
+    public void setPrzystanekwliniId(Long przystanekwliniId) {
+        this.przystanekwliniId = przystanekwliniId;
+    }
 }
