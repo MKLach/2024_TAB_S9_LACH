55400899be34c92701b9571f6c63244132c8984c
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:45:01 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/HarmonogramRepository.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/HarmonogramRepository.java
@@ -1,28 +1,82 @@
 package com.mklachl.sopkom.repository;
+
 import com.mklachl.sopkom.model.entity.Harmonogram;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
 import java.util.List;
-public interface HarmonogramRepository extends CrudRepository<Harmonogram, Short>{
 
+/**
+ * Interfejs repository dla encji Harmonogram
+ */
+public interface HarmonogramRepository extends CrudRepository<Harmonogram, Short> {
+
+    /**
+     * Znajduje wszystkie harmonogramy o określonej nazwie
+     * @param nazwa nazwa
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllByNazwa(String nazwa);
 
+    /**
+     * Znajduje wszystkie harmonogramy, które obowiązują w poniedziałki
+     * @param pon true jeśli obowiązuje, false w przeciwnym razie
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllByPon(boolean pon);
 
+    /**
+     * Znajduje wszystkie harmonogramy, które obowiązują we wtorki
+     * @param wto true jeśli obowiązuje, false w przeciwnym razie
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllByWto(boolean wto);
 
+    /**
+     * Znajduje wszystkie harmonogramy, które obowiązują w środy
+     * @param sro true jeśli obowiązuje, false w przeciwnym razie
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllBySro(boolean sro);
 
+    /**
+     * Znajduje wszystkie harmonogramy, które obowiązują w czwartki
+     * @param czw true jeśli obowiązuje, false w przeciwnym razie
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllByCzw(boolean czw);
 
+    /**
+     * Znajduje wszystkie harmonogramy, które obowiązują w piątki
+     * @param pia true jeśli obowiązuje, false w przeciwnym razie
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllByPia(boolean pia);
 
+    /**
+     * Znajduje wszystkie harmonogramy, które obowiązują w soboty
+     * @param sob true jeśli obowiązuje, false w przeciwnym razie
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllBySob(boolean sob);
 
+    /**
+     * Znajduje wszystkie harmonogramy, które obowiązują w niedziele
+     * @param nie true jeśli obowiązuje, false w przeciwnym razie
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllByNie(boolean nie);
 
+    /**
+     * Znajduje wszystkie harmonogramy o dodatkowych informacjach pasujących do wzorca
+     * @param dodatkoweInf dodatkowe informacje
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAllByDodatkoweInfLike(String dodatkoweInf);
 
+    /**
+     * Znajduje wszystkie harmonogramy
+     * @return Lista harmonogramów
+     */
     List<Harmonogram> findAll();
 }
