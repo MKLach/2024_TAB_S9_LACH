aea5e78dd8f59ee70397cd6d07e82d08446a4898
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:11:30 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/przejazd/PrzejazdDtoOutput.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/przejazd/PrzejazdDtoOutput.java
@@ -3,40 +3,107 @@ package com.mklachl.sopkom.model.dto.przejazd;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mklachl.sopkom.model.dto.PrzejazdKursPrzystanekWliniDto;
 import com.mklachl.sopkom.model.entity.Przejazd;
-import com.mklachl.sopkom.model.entity.PrzejazdKursPrzystanekWlini;
-
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Data Transfer Object (DTO) wyjściowy dla encji Przejazd.
+ */
 public class PrzejazdDtoOutput {
+
+    /**
+     * Identyfikator przejazdu.
+     */
     private Long przejazdId;
+
+    /**
+     * Identyfikator kursu.
+     */
     private Long kursId;
+
+    /**
+     * Numer linii.
+     */
     private String liniaNumer;
-    @JsonFormat(pattern="EE MMM d y H:m:s ZZZ", locale = "en-US")
+
+    /**
+     * Data startu w formacie "EE MMM d y H:m:s ZZZ", locale = "en-US".
+     */
+    @JsonFormat(pattern = "EE MMM d y H:m:s ZZZ", locale = "en-US")
     private Date dataStartu;
-    @JsonFormat(pattern="EE MMM d y H:m:s ZZZ", locale = "en-US")
+
+    /**
+     * Data końca w formacie "EE MMM d y H:m:s ZZZ", locale = "en-US".
+     */
+    @JsonFormat(pattern = "EE MMM d y H:m:s ZZZ", locale = "en-US")
     private Date dataKonca;
-    @JsonFormat(pattern="EE MMM d y H:m:s ZZZ", locale = "en-US")
+
+    /**
+     * Data w formacie "EE MMM d y H:m:s ZZZ", locale = "en-US".
+     */
+    @JsonFormat(pattern = "EE MMM d y H:m:s ZZZ", locale = "en-US")
     private Date data;
-    
-    
+
+    /**
+     * Imię i nazwisko kierowcy.
+     */
     private String kierowcaImieNazwisko;
+
+    /**
+     * Nazwa autobusu.
+     */
     private String autobusNazwa;
+
+    /**
+     * Numer rejestracyjny autobusu.
+     */
     private String autobusNumerRejestracyjny;
+
+    /**
+     * Spalanie.
+     */
     private float spalanie;
+
+    /**
+     * Cena za litr paliwa.
+     */
     private float cenaZaLitr;
+
+    /**
+     * Długość trasy.
+     */
     private float dlugoscTrasy;
+
+    /**
+     * Liczba biletów.
+     */
     private int liczbaBiletow;
+
+    /**
+     * Status przejazdu.
+     */
     private String status;
+
+    /**
+     * Kierunek kursu.
+     */
     private short kierunek;
-    
+
+    /**
+     * Lista przystanków w kursie.
+     */
     private List<PrzejazdKursPrzystanekWliniDto> przystanki;
 
+    /**
+     * Konstruktor domyślny.
+     */
     public PrzejazdDtoOutput() {}
 
+    /**
+     * Konstruktor przyjmujący obiekt encji Przejazd.
+     * @param przejazd Obiekt encji Przejazd.
+     */
     public PrzejazdDtoOutput(Przejazd przejazd) {
         this.przejazdId = przejazd.getPrzejazdId();
         this.kursId = przejazd.getKurs().getKursId();
@@ -44,7 +111,7 @@ public class PrzejazdDtoOutput {
         this.dataStartu = przejazd.getDataStartu();
         this.dataKonca = przejazd.getDataKonca();
         this.kierowcaImieNazwisko = przejazd.getKierowca().getImie() + " " + przejazd.getKierowca().getNazwisko();
-        this.autobusNazwa = przejazd.getAutobus().getNumerRejestracyjny(); //do sprawdzenia
+        this.autobusNazwa = przejazd.getAutobus().getNumerRejestracyjny();
         this.autobusNumerRejestracyjny = przejazd.getAutobus().getNumerRejestracyjny();
         this.spalanie = przejazd.getSpalanie();
         this.cenaZaLitr = przejazd.getCenaZaLitr();
@@ -52,47 +119,38 @@ public class PrzejazdDtoOutput {
         this.liczbaBiletow = przejazd.getPrzejazdBilet().size();
         this.kierunek = przejazd.getKurs().getKierunek().shortValue();
         Date now = new Date();
-        
         this.data = przejazd.getData();
-       
         this.przystanki = przejazd.getPrzejazdKursPrzystanekWlini().stream()
                 .map(PrzejazdKursPrzystanekWliniDto::new).collect(Collectors.toList());
-        
         this.status = "Zaplanowany";
-        
-        int count = 0;
-        
-        for (Iterator<PrzejazdKursPrzystanekWliniDto> iterator = przystanki.iterator(); iterator.hasNext();) {
-			PrzejazdKursPrzystanekWliniDto przejazdKursPrzystanekWliniDto = (PrzejazdKursPrzystanekWliniDto) iterator
-					.next();
-			if(przejazdKursPrzystanekWliniDto.getRealnaGodzinna() != null) {
-				count+=1;
-				 this.status = "W trakcie";
-			}
-			
-			
-		}
-        if(count == przystanki.size()) {
-        	 this.status = "Zakończony";
-        }
-        
-        
-        if (now.after(przejazd.getDataKonca())){
-    		  
-			if( status.equals("Zaplanowany")) {
-				this.status = "Nie odbył się";
-			} else if (status.equals("W trakcie")) {
-				this.status = "Spóźniony";
-			}
-	       
-        } 
-        
-        
-        
-        System.out.println(przystanki.size());
 
+        int count = 0;
+
+        for (PrzejazdKursPrzystanekWliniDto przystanek : przystanki) {
+            if (przystanek.getRealnaGodzinna() != null) {
+                count++;
+                this.status = "W trakcie";
+            }
+        }
+
+        if (count == przystanki.size()) {
+            this.status = "Zakończony";
+        }
+
+        if (now.after(przejazd.getDataKonca())) {
+            if (status.equals("Zaplanowany")) {
+                this.status = "Nie odbył się";
+            } else if (status.equals("W trakcie")) {
+                this.status = "Spóźniony";
+            }
+        }
     }
-    
+
+    /**
+     * Konstruktor przyjmujący obiekt encji Przejazd oraz flagę.
+     * @param przejazd Obiekt encji Przejazd.
+     * @param a Flaga.
+     */
     public PrzejazdDtoOutput(Przejazd przejazd, boolean a) {
         this.przejazdId = przejazd.getPrzejazdId();
         this.kursId = przejazd.getKurs().getKursId();
@@ -107,150 +165,270 @@ public class PrzejazdDtoOutput {
         this.dlugoscTrasy = 0;
         this.liczbaBiletow = 0;
         this.kierunek = przejazd.getKurs().getKierunek().shortValue();
-       
-        
         this.data = przejazd.getData();
-       
         this.przystanki = przejazd.getPrzejazdKursPrzystanekWlini().stream()
                 .map(PrzejazdKursPrzystanekWliniDto::new).collect(Collectors.toList());
-        
         this.status = "Do zaplanowania";
-        
-      
-        
     }
-    
 
     // Getters and Setters
+
+    /**
+     * Zwraca identyfikator przejazdu.
+     * @return przejazdId Identyfikator przejazdu.
+     */
     public Long getPrzejazdId() {
         return przejazdId;
     }
 
+    /**
+     * Ustawia identyfikator przejazdu.
+     * @param przejazdId Identyfikator przejazdu.
+     */
     public void setPrzejazdId(Long przejazdId) {
         this.przejazdId = przejazdId;
     }
 
+    /**
+     * Zwraca identyfikator kursu.
+     * @return kursId Identyfikator kursu.
+     */
     public Long getKursId() {
         return kursId;
     }
 
+    /**
+     * Ustawia identyfikator kursu.
+     * @param kursId Identyfikator kursu.
+     */
     public void setKursId(Long kursId) {
         this.kursId = kursId;
     }
 
+    /**
+     * Zwraca numer linii.
+     * @return liniaNumer Numer linii.
+     */
     public String getLiniaNumer() {
         return liniaNumer;
     }
 
+    /**
+     * Ustawia numer linii.
+     * @param liniaNumer Numer linii.
+     */
     public void setLiniaNumer(String liniaNumer) {
         this.liniaNumer = liniaNumer;
     }
 
+    /**
+     * Zwraca datę startu.
+     * @return dataStartu Data startu.
+     */
     public Date getDataStartu() {
         return dataStartu;
     }
 
+    /**
+     * Ustawia datę startu.
+     * @param dataStartu Data startu.
+     */
     public void setDataStartu(Date dataStartu) {
         this.dataStartu = dataStartu;
     }
 
+    /**
+     * Zwraca datę końca.
+     * @return dataKonca Data końca.
+     */
     public Date getDataKonca() {
         return dataKonca;
     }
 
+    /**
+     * Ustawia datę końca.
+     * @param dataKonca Data końca.
+     */
     public void setDataKonca(Date dataKonca) {
         this.dataKonca = dataKonca;
     }
 
+    /**
+     * Zwraca imię i nazwisko kierowcy.
+     * @return kierowcaImieNazwisko Imię i nazwisko kierowcy.
+     */
     public String getKierowcaImieNazwisko() {
         return kierowcaImieNazwisko;
     }
 
+    /**
+     * Ustawia imię i nazwisko kierowcy.
+     * @param kierowcaImieNazwisko Imię i nazwisko kierowcy.
+     */
     public void setKierowcaImieNazwisko(String kierowcaImieNazwisko) {
         this.kierowcaImieNazwisko = kierowcaImieNazwisko;
     }
 
+    /**
+     * Zwraca nazwę autobusu.
+     * @return autobusNazwa Nazwa autobusu.
+     */
     public String getAutobusNazwa() {
         return autobusNazwa;
     }
 
+    /**
+     * Ustawia nazwę autobusu.
+     * @param autobusNazwa Nazwa autobusu.
+     */
     public void setAutobusNazwa(String autobusNazwa) {
         this.autobusNazwa = autobusNazwa;
     }
 
+    /**
+     * Zwraca numer rejestracyjny autobusu.
+     * @return autobusNumerRejestracyjny Numer rejestracyjny autobusu.
+     */
     public String getAutobusNumerRejestracyjny() {
         return autobusNumerRejestracyjny;
     }
 
+    /**
+     * Ustawia numer rejestracyjny autobusu.
+     * @param autobusNumerRejestracyjny Numer rejestracyjny autobusu.
+     */
     public void setAutobusNumerRejestracyjny(String autobusNumerRejestracyjny) {
         this.autobusNumerRejestracyjny = autobusNumerRejestracyjny;
     }
 
+    /**
+     * Zwraca spalanie.
+     * @return spalanie Spalanie.
+     */
     public float getSpalanie() {
         return spalanie;
     }
 
+    /**
+     * Ustawia spalanie.
+     * @param spalanie Spalanie.
+     */
     public void setSpalanie(float spalanie) {
         this.spalanie = spalanie;
     }
 
+    /**
+     * Zwraca cenę za litr paliwa.
+     * @return cenaZaLitr Cena za litr paliwa.
+     */
     public float getCenaZaLitr() {
         return cenaZaLitr;
     }
 
+    /**
+     * Ustawia cenę za litr paliwa.
+     * @param cenaZaLitr Cena za litr paliwa.
+     */
     public void setCenaZaLitr(float cenaZaLitr) {
         this.cenaZaLitr = cenaZaLitr;
     }
 
+    /**
+     * Zwraca długość trasy.
+     * @return dlugoscTrasy Długość trasy.
+     */
     public float getDlugoscTrasy() {
         return dlugoscTrasy;
     }
 
+    /**
+     * Ustawia długość trasy.
+     * @param dlugoscTrasy Długość trasy.
+     */
     public void setDlugoscTrasy(float dlugoscTrasy) {
         this.dlugoscTrasy = dlugoscTrasy;
     }
 
+    /**
+     * Zwraca liczbę biletów.
+     * @return liczbaBiletow Liczba biletów.
+     */
     public int getLiczbaBiletow() {
         return liczbaBiletow;
     }
 
+    /**
+     * Ustawia liczbę biletów.
+     * @param liczbaBiletow Liczba biletów.
+     */
     public void setLiczbaBiletow(int liczbaBiletow) {
         this.liczbaBiletow = liczbaBiletow;
     }
 
-	public String getStatus() {
-		return status;
-	}
+    /**
+     * Zwraca status przejazdu.
+     * @return status Status przejazdu.
+     */
+    public String getStatus() {
+        return status;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    /**
+     * Ustawia status przejazdu.
+     * @param status Status przejazdu.
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public List<PrzejazdKursPrzystanekWliniDto> getPrzystanki() {
-		return przystanki;
-	}
+    /**
+     * Zwraca listę przystanków w kursie.
+     * @return przystanki Lista przystanków w kursie.
+     */
+    public List<PrzejazdKursPrzystanekWliniDto> getPrzystanki() {
+        return przystanki;
+    }
 
-	public void setPrzystanki(List<PrzejazdKursPrzystanekWliniDto> przystanki) {
-		this.przystanki = przystanki;
-	}
+    /**
+     * Ustawia listę przystanków w kursie.
+     * @param przystanki Lista przystanków w kursie.
+     */
+    public void setPrzystanki(List<PrzejazdKursPrzystanekWliniDto> przystanki) {
+        this.przystanki = przystanki;
+    }
 
-	public Date getData() {
-		return data;
-	}
+    /**
+     * Zwraca datę.
+     * @return data Data.
+     */
+    public Date getData() {
+        return data;
+    }
 
-	public void setData(Date data) {
-		this.data = data;
-	}
+    /**
+     * Ustawia datę.
+     * @param data Data.
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
 
-	public short getKierunek() {
-		return kierunek;
-	}
+    /**
+     * Zwraca kierunek kursu.
+     * @return kierunek Kierunek kursu.
+     */
+    public short getKierunek() {
+        return kierunek;
+    }
 
-	public void setKierunek(short kierunek) {
-		this.kierunek = kierunek;
-	}
+    /**
+     * Ustawia kierunek kursu.
+     * @param kierunek Kierunek kursu.
+     */
+    public void setKierunek(short kierunek) {
+        this.kierunek = kierunek;
+    }
 
-	
     
     /*public String getStatus() {
         return status;
