833058f9fc49d11368b6f830c8aaa29b52320e91
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:44:48 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/BiletRepository.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/BiletRepository.java
@@ -5,12 +5,28 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/**
+ * Interfejs repository dla encji Bilet
+ */
 public interface BiletRepository extends CrudRepository<Bilet, Short> {
 
+    /**
+     * Znajduje wszystkie bilety o określonej cenie
+     * @param cena cena
+     * @return Lista biletów
+     */
     List<Bilet> findAllByCena(float cena);
 
+    /**
+     * Znajduje wszystkie bilety o określonej nazwie
+     * @param nazwa nazwa
+     * @return Lista biletów
+     */
     List<Bilet> findAllByNazwa(String nazwa);
 
+    /**
+     * Znajduje wszystkie bilety
+     * @return Lista biletów
+     */
     List<Bilet> findAll();
-
 }
