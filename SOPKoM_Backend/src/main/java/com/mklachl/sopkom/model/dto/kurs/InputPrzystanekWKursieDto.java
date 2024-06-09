16e3816d2cbe5e3cac926a87190fb822a30fd422
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:02:57 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/kurs/InputPrzystanekWKursieDto.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/kurs/InputPrzystanekWKursieDto.java
@@ -3,28 +3,46 @@ package com.mklachl.sopkom.model.dto.kurs;
 import com.mklachl.sopkom.model.dto.linia.PrzystanekDtoDlaLinia;
 import com.mklachl.sopkom.model.entity.KursPrzystanekWlini;
 
+/**
+ * Data Transfer Object (DTO) wejściowy dla encji KursPrzystanekWlini.
+ */
 public class InputPrzystanekWKursieDto extends AbstractPrzystanekWKursieDto {
 
-	PrzystanekDtoDlaLinia przystanekWLini;
-	
-	public InputPrzystanekWKursieDto() {
-		
-	}
-	
-	public InputPrzystanekWKursieDto(KursPrzystanekWlini kpw) {
-		super(kpw);
-		przystanekWLini = new PrzystanekDtoDlaLinia(kpw.getPrzystanekWlini());
-	}
+    /**
+     * DTO przystanku w linii.
+     */
+    private PrzystanekDtoDlaLinia przystanekWLini;
 
-	public PrzystanekDtoDlaLinia getPrzystanekWLini() {
-		return przystanekWLini;
-	}
+    /**
+     * Konstruktor domyślny.
+     */
+    public InputPrzystanekWKursieDto() {
+    }
 
-	public void setPrzystanekWLini(PrzystanekDtoDlaLinia przystanekWLini) {
-		this.przystanekWLini = przystanekWLini;
-	}
-	
-	
-	
-	
+    /**
+     * Konstruktor przyjmujący obiekt encji KursPrzystanekWlini.
+     * @param kpw Obiekt encji KursPrzystanekWlini.
+     */
+    public InputPrzystanekWKursieDto(KursPrzystanekWlini kpw) {
+        super(kpw);
+        this.przystanekWLini = new PrzystanekDtoDlaLinia(kpw.getPrzystanekWlini());
+    }
+
+    // Getters and Setters
+
+    /**
+     * Zwraca DTO przystanku w linii.
+     * @return przystanekWLini DTO przystanku w linii.
+     */
+    public PrzystanekDtoDlaLinia getPrzystanekWLini() {
+        return przystanekWLini;
+    }
+
+    /**
+     * Ustawia DTO przystanku w linii.
+     * @param przystanekWLini DTO przystanku w linii.
+     */
+    public void setPrzystanekWLini(PrzystanekDtoDlaLinia przystanekWLini) {
+        this.przystanekWLini = przystanekWLini;
+    }
 }
