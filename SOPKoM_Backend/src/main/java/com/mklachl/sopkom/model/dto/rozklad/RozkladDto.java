0c3650bd6dae36429676343901e5ea39a262f946
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:13:11 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/rozklad/RozkladDto.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/rozklad/RozkladDto.java
@@ -1,122 +1,160 @@
 package com.mklachl.sopkom.model.dto.rozklad;
 
 import java.sql.Time;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 import java.util.TreeSet;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.mklachl.sopkom.model.dto.PrzystanekDto;
 import com.mklachl.sopkom.model.dto.kurs.KursDto;
 import com.mklachl.sopkom.model.dto.linia.LiniaDto;
 import com.mklachl.sopkom.model.entity.Przystanek;
 
+/**
+ * Data Transfer Object (DTO) dla rozkładu jazdy.
+ */
 public class RozkladDto {
-	
-	public static class $ implements Comparable<$> {
-		public Time time;
-		public boolean odw;
-		
-		
-		
-		public $(Time time, boolean odw) {
-			super();
-			this.time = time;
-			this.odw = odw;
-		}
-		@Override
-		public int hashCode() {
-			return Objects.hash(odw, time);
-		}
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			$ other = ($) obj;
-			return odw == other.odw && Objects.equals(time, other.time);
-		}
-		
-		public int compareTo($ o) {
-			
-			return this.time.compareTo(o.time);
-		};
-		
-		
-	}
-	
-	
-	PrzystanekDto normalny;
-	
-	@JsonFormat(pattern = "dd.MM.yyyy", timezone = "Europe/Warsaw")
-	Date date;
-	
-	HashMap<String, TreeSet<$>> rozklad = new HashMap<String, TreeSet<$>>();
-	
 
-	public RozkladDto(Przystanek p, KursDto kursDto, Date date) {
-		normalny = new PrzystanekDto(p);
-	
-		
-	}
-	
-	public RozkladDto() {
-		
-		
-	}
+    /**
+     * Klasa wewnętrzna reprezentująca godzinę odjazdu i kierunek kursu.
+     */
+    public static class $ implements Comparable<$> {
+        /**
+         * Czas odjazdu.
+         */
+        public Time time;
 
-	public HashMap<String, TreeSet<$>> getRozklad() {
-		return rozklad;
-	}
+        /**
+         * Flaga wskazująca, czy kurs jest w odwrotnym kierunku.
+         */
+        public boolean odw;
 
-	public void setRozklad(HashMap<String, TreeSet<$>> rozklad) {
-		this.rozklad = rozklad;
-	}
-	
-	
-	public RozkladDto addKurs(LiniaDto linia, Time godzinna, boolean odwrotna) {
-		
-		if(rozklad.containsKey(linia.getNumer())) {
-			
-			rozklad.get(linia.getNumer()).add(new $(godzinna, odwrotna));
-			
-		} else {
-			TreeSet<$> list = new TreeSet<$>();
-			list.add(new $(godzinna, odwrotna));
-			rozklad.put(linia.getNumer(), list);
-			
-		}
-	
-		
-		return this;
-	}
+        /**
+         * Konstruktor przyjmujący czas odjazdu i flagę odwrotności.
+         * @param time Czas odjazdu.
+         * @param odw Flaga odwrotności.
+         */
+        public $(Time time, boolean odw) {
+            this.time = time;
+            this.odw = odw;
+        }
 
-	public PrzystanekDto getNormalny() {
-		return normalny;
-	}
+        @Override
+        public int hashCode() {
+            return Objects.hash(odw, time);
+        }
 
-	public void setNormalny(PrzystanekDto normalny) {
-		this.normalny = normalny;
-	}
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            $ other = ($) obj;
+            return odw == other.odw && Objects.equals(time, other.time);
+        }
 
+        @Override
+        public int compareTo($ o) {
+            return this.time.compareTo(o.time);
+        }
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    /**
+     * Obiekt DTO dla przystanku.
+     */
+    private PrzystanekDto normalny;
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    /**
+     * Data rozkładu w formacie "dd.MM.yyyy".
+     */
+    @JsonFormat(pattern = "dd.MM.yyyy", timezone = "Europe/Warsaw")
+    private Date date;
 
-	
-	
-	
+    /**
+     * Mapa rozkładu, gdzie kluczem jest numer linii, a wartością jest zbiór godzin odjazdów.
+     */
+    private HashMap<String, TreeSet<$>> rozklad = new HashMap<>();
+
+    /**
+     * Konstruktor przyjmujący obiekt Przystanek, KursDto oraz datę.
+     * @param p Obiekt przystanku.
+     * @param kursDto Obiekt DTO kursu.
+     * @param date Data rozkładu.
+     */
+    public RozkladDto(Przystanek p, KursDto kursDto, Date date) {
+        this.normalny = new PrzystanekDto(p);
+        this.date = date;
+    }
+
+    /**
+     * Konstruktor domyślny.
+     */
+    public RozkladDto() {
+    }
+
+    // Getters and Setters
+
+    /**
+     * Zwraca mapę rozkładu.
+     * @return Mapa rozkładu.
+     */
+    public HashMap<String, TreeSet<$>> getRozklad() {
+        return rozklad;
+    }
+
+    /**
+     * Ustawia mapę rozkładu.
+     * @param rozklad Mapa rozkładu.
+     */
+    public void setRozklad(HashMap<String, TreeSet<$>> rozklad) {
+        this.rozklad = rozklad;
+    }
+
+    /**
+     * Dodaje kurs do rozkładu.
+     * @param linia Obiekt DTO linii.
+     * @param godzinna Czas odjazdu.
+     * @param odwrotna Flaga odwrotności.
+     * @return Obiekt RozkladDto.
+     */
+    public RozkladDto addKurs(LiniaDto linia, Time godzinna, boolean odwrotna) {
+        rozklad.computeIfAbsent(linia.getNumer(), k -> new TreeSet<>()).add(new $(godzinna, odwrotna));
+        return this;
+    }
+
+    /**
+     * Zwraca obiekt DTO przystanku.
+     * @return Obiekt DTO przystanku.
+     */
+    public PrzystanekDto getNormalny() {
+        return normalny;
+    }
+
+    /**
+     * Ustawia obiekt DTO przystanku.
+     * @param normalny Obiekt DTO przystanku.
+     */
+    public void setNormalny(PrzystanekDto normalny) {
+        this.normalny = normalny;
+    }
+
+    /**
+     * Zwraca datę rozkładu.
+     * @return Data rozkładu.
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Ustawia datę rozkładu.
+     * @param date Data rozkładu.
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
