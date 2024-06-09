8dcc629597c1885a2a9c29385e6cc171276fde13
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:59:37 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/UserService.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/services/UserService.java
@@ -3,14 +3,35 @@ package com.mklachl.sopkom.services;
 import com.mklachl.sopkom.model.dto.UserDto;
 import com.mklachl.sopkom.model.entity.User;
 
+/**
+ * Interfejs serwisu dla obsługi użytkowników
+ */
 public interface UserService {
 
+    /**
+     * Zapisuje nowego użytkownika
+     * @param user encja użytkownika
+     */
     void saveUser(User user);
     
+    /**
+     * Zapisuje nowego użytkownika na podstawie DTO
+     * @param userDto DTO użytkownika
+     */
     void saveUser(UserDto userDto);
 
+    /**
+     * Znajduje użytkownika po loginie
+     * @param email login
+     * @return znaleziony użytkownik
+     */
     User findUserByLogin(String email);
 
-	User findUserById(Long id);
+    /**
+     * Znajduje użytkownika po ID
+     * @param id ID użytkownika
+     * @return znaleziony użytkownik
+     */
+    User findUserById(Long id);
 
 }
