1ae2a24076e8d7cc5dccee98e86a8a7ba7207e23
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:02:34 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/kurs/InputKursDto.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/kurs/InputKursDto.java
@@ -2,22 +2,37 @@ package com.mklachl.sopkom.model.dto.kurs;
 
 import java.util.List;
 
+/**
+ * Data Transfer Object (DTO) wejściowy dla encji Kurs.
+ */
 public class InputKursDto extends AbstractKursDto {
 
-	public List<InputPrzystanekWKursieDto> przystanki;
-	
-	public InputKursDto() {
-		
-	}
-	
-	public List<InputPrzystanekWKursieDto> getPrzystanki() {
-		return przystanki;
-	}
+    /**
+     * Lista przystanków w kursie.
+     */
+    public List<InputPrzystanekWKursieDto> przystanki;
 
-	public void setPrzystanki(List<InputPrzystanekWKursieDto> przystanki) {
-		this.przystanki = przystanki;
-	}
-	
-	
-	
+    /**
+     * Konstruktor domyślny.
+     */
+    public InputKursDto() {
+    }
+
+    // Getters and Setters
+
+    /**
+     * Zwraca listę przystanków w kursie.
+     * @return przystanki Lista przystanków w kursie.
+     */
+    public List<InputPrzystanekWKursieDto> getPrzystanki() {
+        return przystanki;
+    }
+
+    /**
+     * Ustawia listę przystanków w kursie.
+     * @param przystanki Lista przystanków w kursie.
+     */
+    public void setPrzystanki(List<InputPrzystanekWKursieDto> przystanki) {
+        this.przystanki = przystanki;
+    }
 }
