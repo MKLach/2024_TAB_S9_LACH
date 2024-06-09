d2599f9f486dac333306a8d1f4cd46cdffc1ad40
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:50:24 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/PrzejazdKursPrzystanekWliniRepository.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/PrzejazdKursPrzystanekWliniRepository.java
@@ -8,18 +8,49 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * Interfejs repository dla encji PrzejazdKursPrzystanekWlini
+ */
 public interface PrzejazdKursPrzystanekWliniRepository extends CrudRepository<PrzejazdKursPrzystanekWlini, Long> {
 
+    /**
+     * Znajduje wszystkie kursy przystanku w przejeździe dla danego przejazdu
+     * @param przejazd przejazd
+     * @return Lista kursów przystanku w przejeździe
+     */
     List<PrzejazdKursPrzystanekWlini> findAllByPrzejazd(Przejazd przejazd);
 
+    /**
+     * Znajduje wszystkie kursy przystanku w przejeździe dla danego kursu przystanku w linii
+     * @param kursPrzystanekWlini kurs przystanku w linii
+     * @return Lista kursów przystanku w przejeździe
+     */
     List<PrzejazdKursPrzystanekWlini> findAllByKursPrzystanekWlini(KursPrzystanekWlini kursPrzystanekWlini);
 
+    /**
+     * Znajduje wszystkie kursy przystanku w przejeździe po rzeczywistej godzinie
+     * @param realnaGodzinna rzeczywista godzina
+     * @return Lista kursów przystanku w przejeździe
+     */
     List<PrzejazdKursPrzystanekWlini> findAllByRealnaGodzinna(Date realnaGodzinna);
 
+    /**
+     * Znajduje wszystkie kursy przystanku w przejeździe po rzeczywistej godzinie wcześniejszej niż podana
+     * @param realnaGodzinna rzeczywista godzina
+     * @return Lista kursów przystanku w przejeździe
+     */
     List<PrzejazdKursPrzystanekWlini> findAllByRealnaGodzinnaBefore(Date realnaGodzinna);
 
+    /**
+     * Znajduje wszystkie kursy przystanku w przejeździe po rzeczywistej godzinie późniejszej niż podana
+     * @param realnaGodzinna rzeczywista godzina
+     * @return Lista kursów przystanku w przejeździe
+     */
     List<PrzejazdKursPrzystanekWlini> findAllByRealnaGodzinnaAfter(Date realnaGodzinna);
 
+    /**
+     * Znajduje wszystkie kursy przystanku w przejeździe
+     * @return Lista kursów przystanku w przejeździe
+     */
     List<PrzejazdKursPrzystanekWlini> findAll();
-
 }
