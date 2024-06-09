df5366d139c5dab439fe676e7b1c937c637d3966
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:08:19 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/przejazd/PKPWLDtoUpdate.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/przejazd/PKPWLDtoUpdate.java
@@ -1,37 +1,61 @@
 package com.mklachl.sopkom.model.dto.przejazd;
 
-import java.util.Date;
-import java.util.TimeZone;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
 
+/**
+ * Data Transfer Object (DTO) dla aktualizacji encji PrzejazdKursPrzystanekWLinii.
+ */
 public class PKPWLDtoUpdate {
 
-	Long id;
-	@JsonFormat(pattern = "HH:mm:ss, dd.MM.yyyy", timezone = "UTC")
-	Date date;
-	
-	public PKPWLDtoUpdate() {
-		
-	}
+    /**
+     * Identyfikator encji.
+     */
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    /**
+     * Data w formacie "HH:mm:ss, dd.MM.yyyy", z ustaloną strefą czasową "UTC".
+     */
+    @JsonFormat(pattern = "HH:mm:ss, dd.MM.yyyy", timezone = "UTC")
+    private Date date;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    /**
+     * Konstruktor domyślny.
+     */
+    public PKPWLDtoUpdate() {
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    // Getters and Setters
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	
-	
-	
+    /**
+     * Zwraca identyfikator encji.
+     * @return id Identyfikator encji.
+     */
+    public Long getId() {
+        return id;
+    }
 
+    /**
+     * Ustawia identyfikator encji.
+     * @param id Identyfikator encji.
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Zwraca datę.
+     * @return date Data.
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * Ustawia datę.
+     * @param date Data.
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
