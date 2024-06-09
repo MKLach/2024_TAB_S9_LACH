0d163b4b07776df311d0ff2515caaebaea3a38a8
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:52:22 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/UserRepository.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.mklachl.sopkom.repository;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.mklachl.sopkom.model.entity.User;
 
+/**
+ * Interfejs repository dla encji User
+ */
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    /**
+     * Znajduje użytkownika po loginie
+     * @param login login
+     * @return Użytkownik
+     */
     User findByLogin(String login);
-
 }
