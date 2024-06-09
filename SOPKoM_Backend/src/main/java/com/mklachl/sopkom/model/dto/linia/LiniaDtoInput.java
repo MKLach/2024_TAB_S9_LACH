7594cc2585bc57b9b0c59516d6730349ccd1122a
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:05:44 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/linia/LiniaDtoInput.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/linia/LiniaDtoInput.java
@@ -1,54 +1,54 @@
 package com.mklachl.sopkom.model.dto.linia;
 
 import com.mklachl.sopkom.model.entity.Linia;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Data Transfer Object (DTO) wejściowy dla encji Linia.
+ */
 public class LiniaDtoInput extends LiniaDto {
- 
+
+    /**
+     * Lista przystanków związanych z linią.
+     */
     private List<PrzystanekDtoDlaLiniaInput> przystanki;
 
+    /**
+     * Konstruktor domyślny.
+     */
     public LiniaDtoInput() {
-
     }
 
-    public LiniaDtoInput(Linia Linia) {
-        this.id = Linia.getLiniaId();
-        this.numer = Linia.getNumer();
-        przystanki = new ArrayList<PrzystanekDtoDlaLiniaInput>();
+    /**
+     * Konstruktor przyjmujący obiekt encji Linia.
+     * @param linia Obiekt encji Linia.
+     */
+    public LiniaDtoInput(Linia linia) {
+        super(linia);
+        this.przystanki = new ArrayList<>();
         
-        for(int i = 0; i < Linia.getPrzystankiWlini().size(); i++) {
-        	PrzystanekDtoDlaLiniaInput dto = new PrzystanekDtoDlaLiniaInput(Linia.getPrzystankiWlini().get(i));
-        	this.przystanki.add(dto);
-        	
+        for (int i = 0; i < linia.getPrzystankiWlini().size(); i++) {
+            PrzystanekDtoDlaLiniaInput dto = new PrzystanekDtoDlaLiniaInput(linia.getPrzystankiWlini().get(i));
+            this.przystanki.add(dto);
         }
-        
     }
 
     // Getters and Setters
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNumer() {
-        return numer;
-    }
-
-    public void setNumer(String numer) {
-        this.numer = numer;
-    }
-
+    /**
+     * Zwraca listę przystanków związanych z linią.
+     * @return przystanki Lista przystanków związanych z linią.
+     */
     public List<PrzystanekDtoDlaLiniaInput> getPrzystanki() {
         return przystanki;
     }
 
+    /**
+     * Ustawia listę przystanków związanych z linią.
+     * @param przystanki Lista przystanków związanych z linią.
+     */
     public void setPrzystanki(List<PrzystanekDtoDlaLiniaInput> przystanki) {
         this.przystanki = przystanki;
     }
 }
-
