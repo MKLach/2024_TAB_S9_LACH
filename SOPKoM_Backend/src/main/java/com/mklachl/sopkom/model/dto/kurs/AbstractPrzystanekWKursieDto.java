dff82b28a18952c2e584d978bc94564d5e8d3f8f
Jul1anK <92015143+Jul1anK@users.noreply.github.com>
2024-06-09 20:01:46 +0200

    + comments

--- SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/kurs/AbstractPrzystanekWKursieDto.java
+++ SOPKoM_Backend/src/main/java/com/mklachl/sopkom/model/dto/kurs/AbstractPrzystanekWKursieDto.java
@@ -1,44 +1,71 @@
 package com.mklachl.sopkom.model.dto.kurs;
 
-import java.sql.Time;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.mklachl.sopkom.model.entity.Kurs;
 import com.mklachl.sopkom.model.entity.KursPrzystanekWlini;
+import java.sql.Time;
 
+/**
+ * Abstrakcyjna klasa Data Transfer Object (DTO) dla encji KursPrzystanekWlini.
+ */
 public class AbstractPrzystanekWKursieDto {
 
-	protected Long przystanekwKursieId;
-	
-	@JsonFormat(pattern = "hh:MM:ss")
-	protected Time godzinna;
-	
-	public AbstractPrzystanekWKursieDto() {
-		
-	}
-	
-	public AbstractPrzystanekWKursieDto(KursPrzystanekWlini kpw) {
-		this.godzinna = new Time(kpw.getGodzinna().getTime());
-		this.przystanekwKursieId = kpw.getKursPrzystanekWliniId();
-	}
+    /**
+     * Identyfikator przystanku w kursie.
+     */
+    protected Long przystanekwKursieId;
 
-	public Long getPrzystanekwKursieId() {
-		return przystanekwKursieId;
-	}
+    /**
+     * Godzina przystanku w formacie "hh:MM:ss".
+     */
+    @JsonFormat(pattern = "hh:MM:ss")
+    protected Time godzinna;
 
-	public void setPrzystanekwKursieId(Long przystanekwKursieId) {
-		this.przystanekwKursieId = przystanekwKursieId;
-	}
+    /**
+     * Konstruktor domyślny.
+     */
+    public AbstractPrzystanekWKursieDto() {
+    }
 
-	public Time getGodzinna() {
-		return godzinna;
-	}
+    /**
+     * Konstruktor przyjmujący obiekt encji KursPrzystanekWlini.
+     * @param kpw Obiekt encji KursPrzystanekWlini.
+     */
+    public AbstractPrzystanekWKursieDto(KursPrzystanekWlini kpw) {
+        this.godzinna = new Time(kpw.getGodzinna().getTime());
+        this.przystanekwKursieId = kpw.getKursPrzystanekWliniId();
+    }
 
-	public void setGodzinna(Time godzinna) {
-		this.godzinna = godzinna;
-	}
-	
-	
-	
+    // Getters and Setters
+
+    /**
+     * Zwraca identyfikator przystanku w kursie.
+     * @return przystanekwKursieId Identyfikator przystanku w kursie.
+     */
+    public Long getPrzystanekwKursieId() {
+        return przystanekwKursieId;
+    }
+
+    /**
+     * Ustawia identyfikator przystanku w kursie.
+     * @param przystanekwKursieId Identyfikator przystanku w kursie.
+     */
+    public void setPrzystanekwKursieId(Long przystanekwKursieId) {
+        this.przystanekwKursieId = przystanekwKursieId;
+    }
+
+    /**
+     * Zwraca godzinę przystanku.
+     * @return godzinna Godzina przystanku.
+     */
+    public Time getGodzinna() {
+        return godzinna;
+    }
+
+    /**
+     * Ustawia godzinę przystanku.
+     * @param godzinna Godzina przystanku.
+     */
+    public void setGodzinna(Time godzinna) {
+        this.godzinna = godzinna;
+    }
 }
